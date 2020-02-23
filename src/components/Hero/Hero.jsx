b5b5dbeef6ff0bce4865d1d119223f8583c3d8d6
Anna Dontcova <a.dontcova@gmail.com>
2020-02-23 23:37:05 +0100

    deletes unnecessary text2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -33,7 +33,7 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <a className="cta-btn cta-btn--hero" href="#about">
-              {cta || 'Know more'}
+              {cta}
             </a>
           </p>
         </Fade>
